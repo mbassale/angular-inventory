097d7548534160c0f839f5b6633e4ec3aeebc81d
Marco Bassaletti <bassaletti@gmail.com>
2017-09-27 00:48:19 -0300

    Created product list component to show product listing and also added components to application module.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
