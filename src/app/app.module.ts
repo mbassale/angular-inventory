828738880d3fc17fe8dd7a422c288c400230ae1e
Marco Bassaletti <bassaletti@gmail.com>
2017-09-28 23:40:53 -0300

    Finished product component, added several subcomponents to render image, price and departments.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import {Component, HostBinding, Input, OnInit} from '@angular/core';
-import {Product} from "../product.model";
+import {Product} from '../product.model';
 
 @Component({
   selector: 'app-product',
